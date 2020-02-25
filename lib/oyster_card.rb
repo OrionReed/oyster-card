93303552d15fc481b108561531a6dc89e98c0517
Orion Reed <orionreedt@gmail.com>
2020-02-25 16:56:45 +0000

    step 11

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -5,19 +5,22 @@ class OysterCard
 
   attr_reader :balance
   attr_reader :in_journey
+  attr_reader :entry_station
 
   def initialize
     @balance = 0
     @in_journey = false
+    @entry_station = nil
   end
 
   def top_up(amount)
     @balance += amount unless exceeds_max_balance(amount)
   end
 
-  def touch_in
+  def touch_in(station)
     exceeds_min_balance
     @in_journey = true if @balance >= MIN_BALANCE
+    @entry_station = station
   end
 
   def touch_out
--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -1,6 +1,8 @@
 require 'oyster_card'
 
 RSpec.describe(OysterCard) do
+  let(:dbl_station) { double("Station", name: "test station") }
+
   it { is_expected.to(respond_to(:balance, :top_up, :touch_in, :touch_out)) }
   it { is_expected.to(have_attributes(in_journey: false)) }
 
@@ -18,31 +20,37 @@ RSpec.describe(OysterCard) do
 
   context '#touch_in' do
     it 'raises error when there are insufficient funds' do
-      expect { subject.touch_in }.to(raise_error("Not enough money, please top up"))
+      expect { subject.touch_in(dbl_station) }.to(raise_error("Not enough money, please top up"))
     end
 
     it 'changes status of in_journey to true when in in_journey is false' do
       subject.top_up(10)
-      expect { subject.touch_in }.to(change { subject.in_journey }.to(true))
+      expect { subject.touch_in(dbl_station) }.to(change { subject.in_journey }.to(true))
     end
 
     it 'in_journey stays true when already true' do
       subject.top_up(10)
-      subject.touch_in
-      expect { subject.touch_in }.to_not(change { subject.in_journey })
+      subject.touch_in(dbl_station)
+      expect { subject.touch_in(dbl_station) }.to_not(change { subject.in_journey })
     end
 
     it 'deducts minimum fare' do
       subject.top_up(10)
-      subject.touch_in
+      subject.touch_in(dbl_station)
       expect { subject.touch_out }.to(change { subject.balance }.by(-1))
+    end
+
+    it 'remembers station' do
+      subject.top_up(10)
+      subject.touch_in(dbl_station)
+      expect(subject.entry_station).to(eq(dbl_station))
     end
   end
 
   context '#touch_out' do
     it 'changes status of in_journey to false when in in_journey is true' do
       subject.top_up(10)
-      subject.touch_in
+      subject.touch_in(dbl_station)
       expect { subject.touch_out }.to(change { subject.in_journey }.to(false))
     end
     it 'in_journey stays false when already false' do
